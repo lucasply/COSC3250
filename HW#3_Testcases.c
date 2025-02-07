c62bfd0cc4a5d5f7fac14a3ee21a655135be6d44
lucasply <159568986+lucasply@users.noreply.github.com>
2025-02-07 15:00:08 -0600

    Create HW#3_Testcases.c
    Test cases for HW#3

---
+++ HW#3_Testcases.c
@@ -0,0 +1,47 @@
+/**
+ * @file testcases.c
+ * @provides testcases
+ *
+ * Modified by:
+ *
+ * and
+ *
+ */
+/* Embedded XINU, Copyright (C) 2023.  All rights reserved. */
+
+#include <xinu.h>
+
+/**
+ * testcases - called after initialization completes to test things.
+ */
+void testcases(void)
+{
+    int c;
+
+    kprintf("===TEST BEGIN===\r\n");
+
+    c = kgetc();
+    switch (c)
+    {
+
+        // TODO: Test your operating system!
+    case 'a':
+            kprintf("\r\n123\r\n");
+    case 'b':
+            kprintf("\r\n---32 character test---\r\n");
+
+            kprintf("\r\nabcdefghijklmnopqrstuvwxyz123456732 \r\n");
+
+    case 'c':
+            if(kgetc()){
+            kprintf("\r\n---kgetc() is working---\r\n");
+            }
+
+    default:
+        kprintf("\r\nHello Xinu World!\r\n");
+    }
+
+
+    kprintf("\r\n===TEST END===\r\n");
+    return;
+}

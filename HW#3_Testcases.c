bc39752c120853270853d25e3ff7bc0ce33b4121
lucasply <159568986+lucasply@users.noreply.github.com>
2025-02-07 15:28:54 -0600

    Update HW#3_Testcases.c

--- HW#3_Testcases.c
+++ HW#3_Testcases.c
@@ -27,16 +27,17 @@ void testcases(void)
         // TODO: Test your operating system!
     case 'a':
             kprintf("\r\n123\r\n");
+            break;
     case 'b':
             kprintf("\r\n---32 character test---\r\n");
 
             kprintf("\r\nabcdefghijklmnopqrstuvwxyz123456732 \r\n");
-
+            break;
     case 'c':
             if(kgetc()){
             kprintf("\r\n---kgetc() is working---\r\n");
             }
-
+            break;
     default:
         kprintf("\r\nHello Xinu World!\r\n");
     }

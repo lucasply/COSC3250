f2109912efb9268b95353da64fc8fbf0af13a732
Jonah <156850942+6332harlanj@users.noreply.github.com>
2025-04-28 23:23:07 -0500

    Update xsh_test.c
    Filled in the framework of the testcases

--- HW#9/xsh_test.c
+++ HW#9/xsh_test.c
@@ -6,6 +6,7 @@
 /* Embedded XINU, Copyright (C) 2009.  All rights reserved. */
 
 #include <xinu.h>
+void printFreeList(void);
 
 /**
  * Shell command (test) is testing hook.
@@ -18,25 +19,77 @@ command xsh_test(int nargs, char *args[])
     printf("This is where you should put some testing code.\n");
 
     uchar c;
+	int block, i;
 
     printf("===TEST BEGIN===\n");
-    printf("(0): Test that file delete starts with block 63\n");
-    printf("(1): Test allocating and imediatly freeing all memory one at a time\n");
-    printf("(2): Test alocating all memory\n");
+    printf("0): Test that file delete starts with block 63\n");
+    printf("1): Test allocating and imediatly freeing all memory one at a time\n");
+    printf("2): Test alocating all memory\n");
 
     c = kgetc();
     switch(c) {
         case '0':
 		        // Write a test for checking that file delete starts with the correct file
+				printFreeList();
+				block = sbGetBlock(supertab);
+				printf("Getting Block %d\n", block);
+				
+				sbFreeBlock(supertab, block);
+				printf("Returning Block %d\n", block);
+				
+				printFreeList();
 
                 break;
         case '1':
                 // Write the test case that we did in class
+				block = 0;
+				
+				printFreeList();
+				
+				for(i = 0; i < DISKBLOCKTOTAL; i++) {
+					block = sbGetBlock(supertab);
+					
+					printf("Block %d = %d\n", i, block);
+					sbFreeBlock(supertab, block);
+					printFreeList();
+					
+					if ((i < 5) || ((i > 54) && (i < 63))) {
+						int c = getc(CONSOLE);
+					}
 
                 break;
         case '2':
                 // Write a test case for allocating all memory in the system
-
+				block = 0;
+				
+				blockList[DISKBLOCKTOTAL];
+				
+				printFreeList();
+				
+				printf("Grabbing everything in the free list\n");
+				
+				for(i = 0; i < DISKBLOCKTOTAL; i++) {
+					block = sbGetBlock(supertab);
+					
+					blockList[i] = block;
+					
+					if ((i == 6) || (i == 55) || (i == 64))) {
+						printFreeList();
+					}
+				}
+				
+				printf("Grabed everything from the free list\n");
+				printFreeList();
+				
+				printf("Returning everything to the free list\n");
+				
+				for(i = 0; i < DISKBLOCKTOTAL; i++) {
+					sbFreeBlock(supertab, blockList[i]);
+				}
+				
+				printf("Everything returned to the free list\n");
+				printFreeList();
+				
                 break;
         default:
                 break;
